d908c19306442e1c78ea0b83b115add88ece6474
vishnu datta <dattavishnu34@gmail.com>
2024-03-17 16:31:43 -0500

    this is a simple flask application

---
+++ app.py
@@ -0,0 +1,43 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username, password=password).first()
+        if user:
+            return redirect(url_for('dashboard'))
+        else:
+            return render_template('login.html', message='Invalid credentials')
+    return render_template('login.html')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User(username=username, password=password)
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('login'))
+    return render_template('register.html')
+
+@app.route('/dashboard')
+def dashboard():
+    # Retrieve all users from the database
+    users = User.query.all()
+    return render_template('dashboard.html', users=users)
+
+if __name__ == '__main__':
+    app.run(debug=True)
